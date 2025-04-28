cf690cc7752e8795e6108c3638f3742220ed5535
Inika Bhargava <inikamehro@gmail.com>
2025-04-27 20:07:37 -0700

    Complete create and read operations with UI to begin a birthday app

---
+++ Birthdays/Friend.swift
@@ -0,0 +1,19 @@
+//
+//  Friend.swift
+//  Birthdays
+//
+//  Created by Inika Bhargava on 4/27/25.
+//
+
+import Foundation
+
+class Friend {
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date){
+        self.name = name
+        self.birthday = birthday
+    }
+}
+
