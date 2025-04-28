d7d6d92032538a556289f69398eec7fe093e59eb
Inika Bhargava <inikamehro@gmail.com>
2025-04-27 19:01:12 -0700

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Inika Bhargava on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
