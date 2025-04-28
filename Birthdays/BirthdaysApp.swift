96417655a1c707112f2efbbe1e82cc54cafd4815
Inika Bhargava <inikamehro@gmail.com>
2025-04-27 20:24:23 -0700

    Changed @State values to @Query

--- Birthdays/BirthdaysApp.swift
+++ Birthdays/BirthdaysApp.swift
@@ -6,12 +6,15 @@
 //
 
 import SwiftUI
+import SwiftData
+
 
 @main
 struct BirthdaysApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: Friend.self)
         }
     }
 }
